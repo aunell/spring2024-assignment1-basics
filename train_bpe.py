6276c3642fa8bb840134106deb65bc5ee86e41e6
Alyssa Unell <aunell@mit.edu>
2024-04-13 13:22:07 -0700

    owt

--- train_bpe.py
+++ train_bpe.py
@@ -1,16 +1,17 @@
 from cs336_basics.tokenizers import *
 import json
 
-input_path = '/data/TinyStoriesV2-GPT4-train.txt'
-vocab_size = 10000
-special_tokens = ['<eos>']
+# input_path = '/data/TinyStoriesV2-GPT4-train.txt'
+input_path = '/data/owt_train.txt'
+vocab_size = 32000
+special_tokens = ['<|endoftext|> ']
 
 vocab, merges = bpe_tokenizer_training(input_path, vocab_size, special_tokens)
-# print(vocab)
-# print(merges)
-with open('tinystories_vocab.json', 'w') as json_file:
+print(vocab)
+print(merges)
+with open('owt_vocab.json', 'w') as json_file:
     json.dump(vocab, json_file)
 
-with open('merge.txt', 'w') as file:
+with open('owt_merge.txt', 'w') as file:
     for item in merges:
         file.write("%s\n" % item)
