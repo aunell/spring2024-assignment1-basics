6d2435b913b4b125b630187a8524ec04649f19e4
Alyssa Unell <aunell@mit.edu>
2024-04-13 15:38:05 -0700

    d

--- train_bpe.py
+++ train_bpe.py
@@ -1,17 +1,47 @@
 from cs336_basics.tokenizer_train import *
 import json
+import os
 
-# input_path = '/data/TinyStoriesV2-GPT4-train.txt'
-input_path = '/data/owt_train.txt'
-vocab_size = 32000
-special_tokens = ['<|endoftext|> ']
+dataset = "TinyStoriesV2-GPT4-train"
+#dataset = "owt_train"
 
-vocab, merges = bpe_tokenizer_training(input_path, vocab_size, special_tokens)
-print(vocab)
-print(merges)
-with open('owt_vocab.json', 'w') as json_file:
-    json.dump(vocab, json_file)
+if not os.path.exists(f"data/{dataset}"):
+    os.makedirs(f"data/{dataset}")
 
-with open('owt_merge.txt', 'w') as file:
-    for item in merges:
-        file.write("%s\n" % item)
+print(f"Dataset: {dataset}")
+vocabulary, merges_overall = bpe_tokenizer_training(f"data/{dataset}.txt", 32000, ["<|endoftext|>"])
+    
+#######################################################
+
+# Save vocabulary to json file
+#with open(f"data/{dataset}vocabulary.json", "w") as f:
+#    json.dump(vocabulary, f)
+
+import base64
+int_to_bytes_dict = {}
+for key, value in vocabulary.items():
+    # Convert bytes to base64 encoded string
+    int_to_bytes_dict[key] = base64.b64encode(value).decode('utf-8')
+
+# Save the dictionary to a JSON file
+with open(f"data/{dataset}vocabulary.json", 'w', encoding='utf-8') as file:
+    json.dump(int_to_bytes_dict, file, indent=4)
+
+#######################################################
+
+# Save merges_overall to json file
+#with open(f"data/{dataset}/merges_overall.json", "w") as f:
+#    json.dump(merges_overall, f)
+
+#breakpoint()
+
+# Convert bytes in tuples to base64 strings for JSON serialization
+encoded_tuples = [(base64.b64encode(item[0]).decode('utf-8'), base64.b64encode(item[1]).decode('utf-8')) for item in merges_overall]
+
+with open(f"data/{dataset}/merges_overall.json", 'w', encoding='utf-8') as f:
+    json.dump(encoded_tuples, f, indent=4)
+
+#######################################################
+
+longest_token = max(vocabulary.values(), key=lambda x: len(x) if hasattr(x, '__len__') else 0)
+print("The longest value in the dictionary is:", longest_token)
